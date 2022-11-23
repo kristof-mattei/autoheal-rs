8a03499dc3a635d3cce3a7e3f1454c859cbb5db0
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-11-22 16:08:15 -0800

    fix: add lines to set upstream properly

--- update-from-upstream.sh
+++ update-from-upstream.sh
@@ -8,3 +8,6 @@ git branch update-from-upstream origin/main
 git checkout update-from-upstream
 
 git merge upstream/main
+git branch --set-upstream-to=origin/update-from-upstream update-from-upstream
+
+# git push origin update-from-upstream
