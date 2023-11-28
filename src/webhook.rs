a6f110be9c728b97e39577e78c661341469992c3
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2023-11-28 12:17:44 -0700

    feat: webhooks support ntfy

--- src/webhook.rs
+++ src/webhook.rs
@@ -1,14 +1,50 @@
-use color_eyre::eyre::Context;
+use color_eyre::eyre::Report;
 use http::Request;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::http::HeaderValue;
 use hyper::{Method, Uri};
 use hyper_tls::HttpsConnector;
-use serde_json::json;
+use tracing::{event, Level};
 
 use crate::app_config::AppConfig;
 use crate::http_client::execute_request;
+
+#[derive(Debug)]
+struct WebHookInvocation {
+    url: Uri,
+    container_name: String,
+    container_short_id: String,
+    state: State,
+}
+impl WebHookInvocation {
+    fn to_title(&self) -> &str {
+        match &self.state {
+            State::Success => "Container successfully restarted",
+            State::Failure(_) => "Container failed to restart",
+        }
+    }
+
+    fn to_priority(&self) -> usize {
+        match self.state {
+            State::Success => 3,
+            State::Failure(_) => 5,
+        }
+    }
+
+    fn to_tags(&self) -> &str {
+        match self.state {
+            State::Success => "white_check_mark",
+            State::Failure(_) => "x",
+        }
+    }
+}
+
+#[derive(Debug)]
+enum State {
+    Success,
+    Failure(Report),
+}
 
 pub fn notify_webhook_success(
     app_config: &AppConfig,
@@ -19,71 +55,72 @@ pub fn notify_webhook_success(
         return;
     };
 
-    let message = format!(
-        "Container \"{}\" ({}) found to be unhealthy. Successfully restarted the container!",
-        container_name, container_short_id
-    );
+    let invocation = WebHookInvocation {
+        url: webhook_url,
+        container_name: container_name.to_owned(),
+        container_short_id: container_short_id.to_owned(),
+        state: State::Success,
+    };
 
     tokio::task::spawn(async move {
-        notify_webhook_and_log(&webhook_url, message).await;
+        notify_webhook_and_log(invocation).await;
     });
 }
 
 pub fn notify_webhook_failure(
     app_config: &AppConfig,
-    container_name: &str,
-    container_short_id: &str,
-    error: &color_eyre::Report,
+    container_name: String,
+    container_short_id: String,
+    error: color_eyre::Report,
 ) {
     let Some(webhook_url) = app_config.webhook_url.clone() else {
         return;
     };
 
-    let message = format!(
-        "Container \"{}\" ({}) found to be unhealthy. Failed to restart the container! Error: {:?}",
-        container_name, container_short_id, error
-    );
+    let webhook_config = WebHookInvocation {
+        url: webhook_url,
+        container_name,
+        container_short_id,
+        state: State::Failure(error),
+    };
 
     tokio::task::spawn(async move {
-        notify_webhook_and_log(&webhook_url, message).await;
+        notify_webhook_and_log(webhook_config).await;
     });
 }
 
-async fn notify_webhook_and_log(webhook_url: &Uri, text: String) {
-    match notify_webhook(webhook_url, &text).await {
-        Ok(()) => tracing::info!(
-            message = "Successfully notified webhook",
-            url = ?webhook_url,
-            text = text
-        ),
-        Err(e) => tracing::error!(
-            message = "Failure sending webhook",
-            url = ?webhook_url,
-            text = text,
-            error = ?e
-        ),
+async fn notify_webhook_and_log(invocation: WebHookInvocation) {
+    match notify_webhook(&invocation).await {
+        Ok(()) => event!(Level::TRACE, ?invocation, "Successfully notified webhook"),
+        Err(e) => event!(Level::TRACE, ?invocation, ?e, "Failure sending webhook"),
     };
 }
 
-async fn notify_webhook(webhook_url: &Uri, text: &str) -> Result<(), color_eyre::Report> {
+async fn notify_webhook(invocation: &WebHookInvocation) -> Result<(), color_eyre::Report> {
     let connector = HttpsConnector::new();
 
-    let payload = json!({
-        "text": text,
-    });
-
-    let data = serde_json::to_string(&payload).wrap_err_with(|| "Failed to serialize payload")?;
+    let message = match &invocation.state {
+        State::Success => format!(
+            "Container \"{}\" ({}) was unhealthy, but was successfully restarted.",
+            invocation.container_name, invocation.container_short_id
+        ),
+        State::Failure(error) => format!(
+            "Container \"{}\" ({}) was unhealthy and we failed to restarted it. Please check the logs for more info. \nError: {}",
+            invocation.container_name, invocation.container_short_id, error
+        ),
+    };
 
     let request = Request::builder()
-        .uri(webhook_url)
+        .uri(invocation.url.clone())
         .method(Method::POST)
         .header(
             hyper::header::CONTENT_TYPE,
             HeaderValue::from_static("application/json"),
         )
-        .body(Full::new(Bytes::from(data)))?;
+        .header("X-Title", invocation.to_title())
+        .header("X-Priority", invocation.to_priority())
+        .header("X-Tags", invocation.to_tags())
+        .body(Full::new(Bytes::from(message)))?;
 
-    execute_request(connector, request).await?;
-
-    Ok(())
+    execute_request(connector, request).await.map(|_| ())
 }
