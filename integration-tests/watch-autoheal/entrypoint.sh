023b97db6e934e4f3d602983299b3db604dca35f
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-03-06 15:44:57 -0700

    feat: allow for excluding containers by name

--- integration-tests/watch-autoheal/entrypoint.sh
+++ integration-tests/watch-autoheal/entrypoint.sh
@@ -8,6 +8,7 @@ listenToDockerEvents() {
     docker events --filter 'com.docker.compose.service=should-keep-restarting' --filter 'com.docker.compose.service=shouldnt-restart-*' --filter 'event=restart' | while read -r LOGLINE; do
         echo "$LOGLINE"
         # may be more elaborate checks here.
+        [[ $LOGLINE == *"container restart "*"com.docker.compose.service=ignore-"* && $LOGLINE == *"com.docker.compose.project=$COMPOSE_PROJECT_NAME"* ]] && echo "ERR: No restarts expected on ignore-* containers!" 1>&2 && pkill -9 docker && exit 1
         [[ $LOGLINE == *"container restart "*"com.docker.compose.service=shouldnt-restart-"* && $LOGLINE == *"com.docker.compose.project=$COMPOSE_PROJECT_NAME"* ]] && echo "ERR: No restarts expected on shouldnt-restart-* containers!" 1>&2 && pkill -9 docker && exit 1
         [[ $LOGLINE == *"container restart "*"com.docker.compose.service=should-keep-restarting"* && $LOGLINE == *"com.docker.compose.project=$COMPOSE_PROJECT_NAME"* ]] && echo "OK: Expected restart on should-keep-restarting container!" && pkill -9 docker && expected_restarts=$((expected_restarts + 1))
         [[ $expected_restarts == 1 ]] && echo "OK: All expected restarts happened" && exit 0
