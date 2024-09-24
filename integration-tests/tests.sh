164dd986987a326254cbbd0170903da31cb689fb
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-09-24 13:01:45 -0700

    fix: docker-compose has been deprecated for docker compose

--- integration-tests/tests.sh
+++ integration-tests/tests.sh
@@ -16,25 +16,25 @@ fi
 
 export COMPOSE_FILE
 
-docker-compose config
+docker compose config
 
 function cleanup() {
     exit_status=$?
     echo "exit was $exit_status"
     # stop autoheal first, to stop it restarting the test containers while we try to stop them
-    docker-compose stop autoheal
-    docker-compose down || true
+    docker compose stop autoheal
+    docker compose down || true
     exit "$exit_status"
 }
 
 trap cleanup EXIT
-docker-compose build
-docker-compose up --no-start --quiet-pull --force-recreate
+docker compose build
+docker compose up --no-start --quiet-pull --force-recreate
 
-docker-compose start should-keep-restarting
-docker-compose start shouldnt-restart-healthy
-docker-compose start shouldnt-restart-no-label
-docker-compose start ignore
-docker-compose start autoheal
+docker compose start should-keep-restarting
+docker compose start shouldnt-restart-healthy
+docker compose start shouldnt-restart-no-label
+docker compose start ignore
+docker compose start autoheal
 
-docker-compose up --abort-on-container-exit --exit-code-from watch-autoheal watch-autoheal
+docker compose up --abort-on-container-exit --exit-code-from watch-autoheal watch-autoheal
